65ad221bf904404ff0ea5299b5f8bdecd452ac28
sulhicader <sulhicader1@gmail.com>
2020-07-02 19:06:38 +0530

    port changed

--- app.py
+++ app.py
@@ -102,4 +102,4 @@ app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
 
 if __name__ == "__main__":
     app.debug=False
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', port=80)
